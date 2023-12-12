8d455ee4c86342aa45a1ad5610bbe92ae0e52670
Fabrizio <fabrinucci99@gmail.com>
2023-12-11 21:14:36 -0300

    Add get all products and products by id

--- pruebas/02-bazar-universal/backend/index.ts
+++ pruebas/02-bazar-universal/backend/index.ts
@@ -1,10 +1,23 @@
 import express, { Express, Request, Response } from 'express';
+import productsData from './data/products.json';
 
 const app: Express = express();
 const PORT = 4000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('<h1>Hello world</h1>');
+  res.json(productsData);
+});
+
+app.get('/products/:id', (req, res) => {
+  const productId = req.params.id;
+  const product = productsData.products.find(
+    (product) => product.id === parseInt(productId)
+  );
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).send('Product not found');
+  }
 });
 
 app.listen(PORT, () => {
