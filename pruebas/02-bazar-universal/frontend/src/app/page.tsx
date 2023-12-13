76f6ec983088a560a71d2587eedd158326800599
Fabrizio <fabrinucci99@gmail.com>
2023-12-13 19:36:05 -0300

    Add handleSearch

--- pruebas/02-bazar-universal/frontend/src/app/page.tsx
+++ pruebas/02-bazar-universal/frontend/src/app/page.tsx
@@ -1,12 +1,25 @@
+'use client';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { FormEvent, useState } from 'react';
 
 export default function Home() {
+  const [search, setSearch] = useState('');
+  const router = useRouter();
+  const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (search === '') return;
+    router.push(`results/${search.toLowerCase()}`);
+  };
   return (
     <main className='flex min-h-screen flex-col items-center justify-center p-24'>
       <Image src='/logo.webp' alt='Logo' width={150} height={150} />
       <h1 className='text-4xl'>Bazar Online</h1>
-      <form className='flex flex-col gap-6 mt-6'>
+      <form onClick={handleSearch} className='flex flex-col gap-6 mt-6'>
         <input
+          onChange={(e) => setSearch(e.target.value)}
+          name='search'
+          value={search}
           className='p-2 text-black rounded-md'
           type='text'
           placeholder='laptops, smartphones'
